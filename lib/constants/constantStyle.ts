22e98f7901e25a032bd20793d166978b310eec22
MDV8 <yesid.sacaca.mc4@ypfbrefinacion.com.bo>
2026-05-21 23:26:39 -0400

    change in style

--- lib/constants/constantStyle.ts
+++ lib/constants/constantStyle.ts
@@ -1,20 +1,20 @@
 const CURSOR_POINTER = ` py-2 px-5 w-full cursor-pointer `;
 
 export const STYLE_INTERNAL = {
-    primary: ` bg-rest-primary text-gray-400 hover:bg-rest-secondary hover:text-white ${CURSOR_POINTER}`,
-    primaryRed: ` bg-rest-primary text-gray-400 hover:bg-rest-red hover:text-white ${CURSOR_POINTER}`,
-    secondary: ` bg-rest-secondary text-rest-primary hover:bg-rest-secondary hover:text-white ${CURSOR_POINTER}`,
-    red: ` bg-rest-red text-white ${CURSOR_POINTER}`,
-    white: ` bg-white text-rest-primary w-full outline-1 outline-rest-primary ${CURSOR_POINTER}`,
-    cancelGray: ` bg-slate-200 text-slate-700 w-full  ${CURSOR_POINTER}`,
-    headerModalPrimary: `
+  primary: ` bg-rest-primary text-gray-400 hover:bg-rest-secondary hover:text-white ${CURSOR_POINTER}`,
+  primaryRed: ` bg-rest-primary text-gray-400 hover:bg-rest-red hover:text-white ${CURSOR_POINTER}`,
+  secondary: ` bg-rest-secondary text-rest-primary hover:bg-rest-secondary hover:text-white ${CURSOR_POINTER}`,
+  red: ` bg-rest-red text-white ${CURSOR_POINTER}`,
+  white: ` bg-white text-rest-primary w-full outline-1 outline-rest-primary ${CURSOR_POINTER}`,
+  cancelGray: ` bg-slate-200 text-slate-700 w-full  ${CURSOR_POINTER}`,
+  headerModalPrimary: `
   bg-gradient-to-r 
   from-rest-primary-down 
   via-rest-primary
   to-rest-primary-up
 `,
 
-confirmModalPrimary: `
+  confirmModalPrimary: `
   flex items-center gap-2
   justify-center
   
@@ -36,7 +36,7 @@ confirmModalPrimary: `
 
   ${CURSOR_POINTER}
 `,
-cancelModalRed: `
+  cancelModalRed: `
   flex items-center gap-2
   justify-center
   
@@ -59,15 +59,15 @@ cancelModalRed: `
   ${CURSOR_POINTER}
 `,
 
-//   border-2 border-transparent
-//   hover:bg-white 
-//   hover:text-rest-red 
-//   hover:border-rest-red
+  //   border-2 border-transparent
+  //   hover:bg-white
+  //   hover:text-rest-red
+  //   hover:border-rest-red
 
-    // before: ` ${STYLE_ROOT.before} px-5 py-2 w-full `,
-    // onlyPrimary: ` ${STYLE_ROOT.primary} `,
+  // before: ` ${STYLE_ROOT.before} px-5 py-2 w-full `,
+  // onlyPrimary: ` ${STYLE_ROOT.primary} `,
 
-    // buttonBase: " inline-flex items-center justify-center transition-all active:scale-95 font-medium cursor-pointer",
-    // button: " bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 ",
-    // input: "w-full border-2 border-gray-100 p-2 rounded-md focus:border-blue-400 outline-none transition-colors"
+  // buttonBase: " inline-flex items-center justify-center transition-all active:scale-95 font-medium cursor-pointer",
+  // button: " bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 ",
+  // input: "w-full border-2 border-gray-100 p-2 rounded-md focus:border-blue-400 outline-none transition-colors"
 };
